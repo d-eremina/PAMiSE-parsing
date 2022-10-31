14891387423d739b9f91f079f816579ddc90fad2
Daria <59935825+d-eremina@users.noreply.github.com>
2022-10-31 23:17:46 +0300

    Update aggregator.py

--- logs/aggregator.py
+++ logs/aggregator.py
@@ -11,3 +11,15 @@ for file in os.listdir(folder):
     data = pd.concat([data,x],axis=0)
 
 data = data.sort_values(by=["timestamp"], ascending=True)
+data["url"] = data["url"].astype('string').fillna("")
+data["request"] = data["request"].astype('string').fillna("")
+data["response"] = data["response"].astype('string').fillna("")
+data["message"] = data["message"].astype('string').fillna("")
+data["status"] = data["status"].fillna(0).astype('int32')
+data["elapsed"] = data["elapsed"].fillna(0)
+
+with open("./summary.csv", 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=';')
+    writer.writerow(["timestamp", "url", "status", "elapsed", "request", "response", "message"])
+    for _, row in data.iterrows():
+        writer.writerow(row)
